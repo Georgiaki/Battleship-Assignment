d202f7fe8c28a31df61a86716887638aa37afc33
George <george.demertzis1999@gmail.com>
2022-05-10 22:13:03 +1000

    Added grid with variables to adjust size

--- main.js
+++ main.js
@@ -1,3 +1,170 @@
+//import * as THREE from '/js/three.js';
+//import {OrbitControls} from '/js/OrbitControls.js';
+import {GUI} from './js/dat.gui.module.js'
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new THREE.OrbitControls(camera, renderer.domElement);
+
+
+camera.position.set(10, 15, -22);
+
+orbit.update();
+
+// select ship size
+var battleshipLength = 2;
+var xoffset = 0;
+var zoffset = 0.5;
+
+
+
+
+const planeMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        visible: false
+    })
+);
+planeMesh.rotateX(-Math.PI / 2);
+scene.add(planeMesh);
+planeMesh.name = 'ground';
+
+const grid = new THREE.GridHelper(20, 20);
+scene.add(grid);
+
+const highlightMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(battleshipLength, 1),
+    new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        transparent: true
+    })
+);
+highlightMesh.rotateX(-Math.PI / 2);
+highlightMesh.position.set(0.5, 0, 0.5);
+scene.add(highlightMesh);
+
+
+const mousePosition = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+let intersects;
+
+window.addEventListener('mousemove', function(e) {
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mousePosition, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    intersects.forEach(function(intersect) {
+        if(intersect.object.name === 'ground') {
+            const highlightPos = new THREE.Vector3().copy(intersect.point).floor();
+            highlightMesh.position.set(highlightPos.x+xoffset, 0, highlightPos.z+zoffset);
+
+            const objectExist = objects.find(function(object) {
+                return (object.position.x === highlightMesh.position.x)
+                && (object.position.z === highlightMesh.position.z)
+            });
+
+            if(!objectExist)
+                highlightMesh.material.color.setHex(0xFFFFFF);
+            else
+                highlightMesh.material.color.setHex(0xFF0000);
+        }
+    });
+});
+
+const sphereMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(0.4, 4, 2),
+    new THREE.MeshBasicMaterial({
+        wireframe: true,
+        color: 0xFFEA00
+    })
+);
+
+const objects = [];
+
+window.addEventListener('mousedown', function() {
+    const objectExist = objects.find(function(object) {
+        return (object.position.x === highlightMesh.position.x)
+        && (object.position.z === highlightMesh.position.z)
+    });
+
+    if(!objectExist) {
+        intersects.forEach(function(intersect) {
+            if(intersect.object.name === 'ground') {
+                const sphereClone = sphereMesh.clone();
+                sphereClone.position.copy(highlightMesh.position);
+                scene.add(sphereClone);
+                objects.push(sphereClone);
+                highlightMesh.material.color.setHex(0xFF0000);
+            }
+        });
+    }
+    console.log(scene.children.length);
+});
+
+function animate(time) {
+    highlightMesh.material.opacity = 1 + Math.sin(time / 120);
+    objects.forEach(function(object) {
+        object.rotation.x = time / 1000;
+        object.rotation.z = time / 1000;
+        object.position.y = 0.5 + 0.5 * Math.abs(Math.sin(time / 1000));
+    });
+    renderer.render(scene, camera);
+
+
+
+
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// GUI //
+const gui = new GUI();
+gui.add(highlightMesh.scale, 'x', 1, 5);
+
+switch (battleshipLength) {
+	case 2:
+		xoffset = 0;
+		zoffset = 0.5;
+	  break;
+
+	case 3:
+		xoffset = 0.5;
+		zoffset = 0.5;
+	  break;
+
+	case 4:
+		xoffset = 0;
+		zoffset = 0.5;
+	  break;
+
+	case 5:
+		xoffset = 0.5;
+		zoffset = 0.5;
+	  break;
+  };
+
+
+
 /*// GLOBAL VARIABLES  //
 const gridRows = 4;
 var countRows = 0;
